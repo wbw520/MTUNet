5127359e72e68df787fe0002c8d5f1fc380de74b
Liangzhi Li <conscienceli@gmail.com>
2020-10-20 15:52:28 +0900

    add test_fsl.py

---
+++ test_fsl.py
@@ -0,0 +1,90 @@
+import argparse
+from train import get_args_parser
+from torch.utils.data import DataLoader, Dataset
+import matplotlib.cm as mpl_color_map
+import copy
+from model.FSL_similarity import FSLSimilarity
+from loaders.base_loader import make_loaders
+from PIL import Image
+import numpy as np
+import torch
+
+
+def test(args, model, img, image, record_name):
+    image = image.to(device, dtype=torch.float32)
+    output = model(image)
+
+    #For vis
+    image_raw = img
+    image_raw.save('vis/image.png')
+    image_raw.save("vis/" + record_name + "image.png")
+    model.train()
+
+    for id in range(args.num_slot):
+        image_raw = Image.open('vis/image.png').convert('RGB')
+        slot_image = np.array(Image.open(f'vis/slot_{id}.png').resize(image_raw.size, resample=Image.BILINEAR), dtype=np.uint8)
+
+        heatmap_only, heatmap_on_image = apply_colormap_on_image(image_raw, slot_image, 'jet')
+        heatmap_on_image.save("vis/" + record_name + f'slot_mask_{id}.png')
+
+
+def apply_colormap_on_image(org_im, activation, colormap_name):
+    """
+        Apply heatmap on image
+    Args:
+        org_img (PIL img): Original image
+        activation_map (numpy arr): Activation map (grayscale) 0-255
+        colormap_name (str): Name of the colormap
+    """
+    # Get colormap
+    color_map = mpl_color_map.get_cmap(colormap_name)
+    no_trans_heatmap = color_map(activation)
+    # Change alpha channel in colormap to make sure original image is displayed
+    heatmap = copy.copy(no_trans_heatmap)
+    heatmap[:, :, 3] = 0.4
+    heatmap = Image.fromarray((heatmap*255).astype(np.uint8))
+    no_trans_heatmap = Image.fromarray((no_trans_heatmap*255).astype(np.uint8))
+
+    # Apply heatmap on iamge
+    heatmap_on_image = Image.new("RGBA", org_im.size)
+    heatmap_on_image = Image.alpha_composite(heatmap_on_image, org_im.convert('RGBA'))
+    heatmap_on_image = Image.alpha_composite(heatmap_on_image, heatmap)
+    return no_trans_heatmap, heatmap_on_image
+
+
+def main():
+    model = FSLSimilarity(args)
+    model_name = "similarity_checkpoint_ab_att1.pth"
+    checkpoint = torch.load(f"{args.output_dir}/" + model_name, map_location=args.device)
+    model.load_state_dict(checkpoint["model"])
+    model.to(device)
+    model.eval()
+
+    dataset_val = make_loaders(args)["val"]
+    data = iter(dataset_val).next()
+    inputs_query = data["query"]["image"][0]
+    query_name = data["query"]["name"]
+    inputs_support = data["support"]["image"][0]
+    support_name = data["support"]["name"]
+    cls = data["selected_cls"][0]
+    print(cls)
+    for i in range(len(inputs_support)):
+        iters(support_name[i], model, torch.unsqueeze(inputs_support[i], dim=0), "support/pic_" + str(cls[i].item()) + "_")
+    print("------------")
+    for j in range(len(inputs_query)):
+        iters(query_name[j], model, torch.unsqueeze(inputs_query[j], dim=0), "query/pic_" + str(cls[j].item()) + "_")
+
+
+def iters(name, model, image, record_name):
+    image_orl = Image.open(name[0])
+    test(args, model, image_orl, image, record_name)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('model test script', parents=[get_args_parser()])
+    args = parser.parse_args()
+    args.batch_size = 1
+    args.query = 1
+    args.vis = True
+    device = torch.device(args.device)
+    main()
