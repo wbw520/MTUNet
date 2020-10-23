d0bbcb2750ac43b8fa04179bd16259316d241cd6
Liangzhi Li <conscienceli@gmail.com>
2020-10-23 10:21:59 +0900

    use loss in test

--- test_fsl.py
+++ test_fsl.py
@@ -3,7 +3,7 @@ from train import get_args_parser
 from torch.utils.data import DataLoader, Dataset
 import matplotlib.cm as mpl_color_map
 import copy
-from model.FSL_similarity import FSLSimilarity
+from model.FSL_similarity import FSLSimilarity, SimilarityLoss
 from loaders.base_loader import make_loaders
 from PIL import Image
 import numpy as np
@@ -16,7 +16,7 @@ os.makedirs('vis/query', exist_ok=True)
 
 def test(args, model, img, image, record_name):
     image = image.to(device, dtype=torch.float32)
-    output = model(image)
+    output, att = model(image)
 
     #For vis
     image_raw = img
@@ -31,6 +31,7 @@ def test(args, model, img, image, record_name):
         heatmap_only, heatmap_on_image = apply_colormap_on_image(image_raw, slot_image, 'jet')
         heatmap_on_image.save("vis/" + record_name + f'slot_mask_{id}.png')
 
+    return output
 
 def apply_colormap_on_image(org_im, activation, colormap_name):
     """
@@ -55,34 +56,37 @@ def apply_colormap_on_image(org_im, activation, colormap_name):
     heatmap_on_image = Image.alpha_composite(heatmap_on_image, heatmap)
     return no_trans_heatmap, heatmap_on_image
 
-
 def main():
     model = FSLSimilarity(args)
-    model_name = "similarity_checkpoint_ab4_att0.pth"
+    model_name = "similarity_checkpoint_ab10xiao_att0.pth"
     checkpoint = torch.load(f"{args.output_dir}/" + model_name, map_location=args.device)
     model.load_state_dict(checkpoint["model"])
     model.to(device)
     model.eval()
-
     dataset_val = make_loaders(args)["val"]
     data = iter(dataset_val).next()
     inputs_query = data["query"]["image"][0]
     query_name = data["query"]["name"]
+    labels_query = data["query"]["label"].to(device, dtype=torch.int64)
     inputs_support = data["support"]["image"][0]
     support_name = data["support"]["name"]
     cls = data["selected_cls"][0]
     print(cls)
+    total_out = []
     for i in range(len(inputs_support)):
-        iters(support_name[i], model, torch.unsqueeze(inputs_support[i], dim=0), "support/pic_" + str(cls[i].item()) + "_")
+        total_out.append(iters(support_name[i], model, torch.unsqueeze(inputs_support[i], dim=0), "support/pic_" + str(cls[i].item()) + "_"))
     print("------------")
     for j in range(len(inputs_query)):
-        iters(query_name[j], model, torch.unsqueeze(inputs_query[j], dim=0), "query/pic_" + str(cls[j].item()) + "_")
-
+        total_out.append(iters(query_name[j], model, torch.unsqueeze(inputs_query[j], dim=0), "query/pic_" + str(cls[j].item()) + "_"))
+    pp = torch.cat(total_out, dim=0)
+    # print(labels_query.size())
+    loss, acc = criterion(pp, 0, labels_query, 0, "val")
+    # print(pp.size())
 
 def iters(name, model, image, record_name):
     image_orl = Image.open(name[0])
-    test(args, model, image_orl, image, record_name)
-
+    out = test(args, model, image_orl, image, record_name)
+    return out
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('model test script', parents=[get_args_parser()])
@@ -91,4 +95,5 @@ if __name__ == '__main__':
     args.query = 1
     args.vis = True
     device = torch.device(args.device)
+    criterion = SimilarityLoss(args)
     main()
